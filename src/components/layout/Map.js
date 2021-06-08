1439c8459c5bf117e8803d83a5cd37af54b3c474
Loc Nguyen <kten1112@gmail.com>
2021-06-08 09:32:09 +0700

    #24 Show pop-up content for hydroelectric constructs

--- src/components/layout/Map.js
+++ src/components/layout/Map.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { trackPromise } from 'react-promise-tracker';
 import axios from "axios";
 import { MapContainer, TileLayer, LayersControl, Marker, Popup } from "react-leaflet";
@@ -21,27 +22,18 @@ export default class Map extends React.Component {
 		this.state = {
 			center: [21.529737201190642, 103.9692398828125],
 			zoom: 8,
-			mainHydroelectricLocations: []
+			mainHydroelectricInfo: [],
 		};
 	}
 	
 	componentDidMount(){
 		trackPromise(
 			axios
-				.get(configData.API_URL + "/quan-ly-cap-phep/nuoc-mat/danh-sach-giay-phep-thuy-dien")
+				.get(configData.API_URL + "/quan-ly-cap-phep/nuoc-mat/thong-tin-ban-do-thuy-dien")
 				.then((response) => {
 					if(response.status === 200)
 					{	
-						var locations = [];
-						response.data.map((e, k) => {
-							e.hang_muc_ct.map((i) => {
-								if(i.toa_do_chinh == 1)
-								{
-									locations.push(i);
-								}
-							})
-						})
-						this.setState({mainHydroelectricLocations: locations})
+						this.setState({mainHydroelectricInfo: response.data});
 					}
 				})
 				.catch((error) => {
@@ -49,9 +41,17 @@ export default class Map extends React.Component {
 				})
 			);
 	}
+
+	formatDate(date) {
+        var date_format = new Date(date);
+        var d = date_format.getDate();
+        var m = date_format.getMonth();
+        var y = date_format.getFullYear();
+        return '' + (d <= 9 ? '0' + d : d) + '/' + (m <= 9 ? '0' + m : m) + '/' + y;
+    }
 	
     render() {
-		const {mainHydroelectricLocations} = this.state;
+		const {mainHydroelectricInfo} = this.state;
 		
 		return (
 			<MapContainer className="h-100 w-100 position-relative" center={this.state.center} zoom={this.state.zoom}>
@@ -77,13 +77,52 @@ export default class Map extends React.Component {
 					</LayersControl.BaseLayer>
 
 					{(this.props.pagename === "thuy-dien") ?
-						mainHydroelectricLocations.map((marker, key) => (
-							<Marker position={[marker.longitude, marker.latitude]} key={key} >
+						mainHydroelectricInfo.map((marker, key) => (
+							<Marker position={[marker["0"].longitude, marker["0"].latitude]} key={key} >
 								<Popup>
-									<b>Nội dung</b>
+								<div>
+									<h5 class="card-title fw-bold font-13">{marker["0"].ten_hang_muc+" - "+marker.ten_ct}</h5>
+									<table class="table table-striped table-hover">
+										<tbody>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Tọa độ X</td>
+												<td class="col-8 py-1">{marker["0"].toa_do_x}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Tọa độ Y</td>
+												<td class="col-8 py-1">{marker["0"].toa_do_y}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Địa điểm</td>
+												<td class="col-8 py-1">{marker.dia_diem}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Số GP</td>
+												<td class="col-8 py-1">{marker.so_gp}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Ngày cấp</td>
+												<td class="col-8 py-1">{this.formatDate(marker.thoi_gian_cp)}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1 font-11">Cấp thẩm quyền</td>
+												<td class="col-8 py-1">{marker.don_vi_tham_quyen}</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Q <sub>xả TT</sub>  gp</td>
+												<td class="col-8 py-1">{marker.luu_luong_xa_toi_thieu} m<sup>3</sup>/s</td>
+											</tr>
+											<tr class="col-12 d-flex p-0">
+												<td class="col-4 py-1">Q <sub>xả TT</sub>  thực tế</td>
+												<td class="col-8 py-1"></td>
+											</tr>
+										</tbody>
+									</table>
+									<Link to={'/quan-ly-cap-phep/nuoc-mat/thuy-dien/xem-thong-tin-chung/'+marker.id} class="card-link">Chi tiết công trình</Link>
+								</div>
 								</Popup>
 							</Marker>
-							))
+						))
 						: ""
 					}
 				</LayersControl>
