8ec0b64dcc2f57d74ebdb1c8ad0545d2c796c392
Loc Nguyen <kten1112@gmail.com>
2021-06-22 11:35:41 +0700

    #35 Update map use Imagery Esri basemap

--- src/components/layout/Map.js
+++ src/components/layout/Map.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { trackPromise } from 'react-promise-tracker';
 import axios from "axios";
 import { MapContainer, TileLayer, LayersControl, Marker, Popup } from "react-leaflet";
+import { BasemapLayer } from "react-esri-leaflet";
 import L from 'leaflet';
 
 import configData from '../../config.json';
@@ -22,130 +23,15 @@ export default class Map extends React.Component {
 		this.state = {
 			center: [21.529737201190642, 103.9692398828125],
 			zoom: 8,
-			mainHydroelectricInfo: [],
-			itemHydroelectricInfo: [],
-			idConstruct: this.props.idConstruct,
-			subpagename: this.props.subpagename
 		};
 	}
 	
-	componentDidMount(){
-		trackPromise(
-			axios
-				.get(configData.API_URL + "/quan-ly-cap-phep/nuoc-mat/thong-tin-ban-do-thuy-dien")
-				.then((response) => {
-					if(response.status === 200)
-					{	
-						this.setState({mainHydroelectricInfo: response.data});
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				})
-			);
-
-		trackPromise(
-			axios
-				.get(configData.API_URL + "/quan-ly-cap-phep/nuoc-mat/giay-phep-thuy-dien/"+this.state.idConstruct)
-				.then((response) => {
-					if(response.status === 200)
-					{
-						
-						this.setState({itemHydroelectricInfo: response.data.hang_muc_ct})
-					}
-				})
-				.catch((error) => {
-					this.setState({msg: error.response})
-				})
-			)
-	}
-
-	formatDate(date) {
-        var date_format = new Date(date);
-        var d = date_format.getDate();
-        var m = date_format.getMonth();
-        var y = date_format.getFullYear();
-        return '' + (d <= 9 ? '0' + d : d) + '/' + (m <= 9 ? '0' + m : m) + '/' + y;
-    }
-	
     render() {
-		const {mainHydroelectricInfo, itemHydroelectricInfo} = this.state;
 		
 		return (
 			<MapContainer className="h-100 w-100 position-relative" center={this.state.center} zoom={this.state.zoom}>
-				<LayersControl position="topright">
-					<LayersControl.BaseLayer checked name="Bản đồ chính">
-						<TileLayer
-						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-						/>
-						
-					</LayersControl.BaseLayer>
-					<LayersControl.BaseLayer name="Bản đồ xám">
-						<TileLayer
-						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-						url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-						/>
-					</LayersControl.BaseLayer>
-					<LayersControl.BaseLayer name="Bản đồ đường">
-						<TileLayer
-						attribution='&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-						url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
-						/>
-					</LayersControl.BaseLayer>
-
-					{(this.props.pagename === "thuy-dien" && this.props.subpagename === "") ?
-						mainHydroelectricInfo.map((marker, key) => (
-							<Marker position={[marker.hang_muc_ct[0].longitude, marker.hang_muc_ct[0].latitude]} key={key} >
-								<Popup>
-								<div>
-									<h5 className="card-title fw-bold font-13">{marker.hang_muc_ct[0].tenhangmuc+" - "+marker.congtrinh_ten}</h5>
-									<table className="table table-striped table-hover">
-										<tbody>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Tọa độ X</td>
-												<td className="col-8 py-1">{marker.hang_muc_ct[0].x}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Tọa độ Y</td>
-												<td className="col-8 py-1">{marker.hang_muc_ct[0].y}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Địa điểm</td>
-												<td className="col-8 py-1">{marker.congtrinh_diadiem}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Số GP</td>
-												<td className="col-8 py-1">{marker.gp_sogiayphep}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Ngày cấp</td>
-												<td className="col-8 py-1">{this.formatDate(marker.gp_thoigiancapphep)}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1 font-11">Cấp thẩm quyền</td>
-												<td className="col-8 py-1">{marker.gp_donvi_thamquyen}</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Q <sub>xả TT</sub>  gp</td>
-												<td className="col-8 py-1">{marker.luuluong_xadongchay_toithieu} m<sup>3</sup>/s</td>
-											</tr>
-											<tr className="col-12 d-flex p-0">
-												<td className="col-4 py-1">Q <sub>xả TT</sub>  thực tế</td>
-												<td className="col-8 py-1"></td>
-											</tr>
-										</tbody>
-									</table>
-									<Link to={'/quan-ly-cap-phep/nuoc-mat/thuy-dien/xem-thong-tin-chung/'+marker.id} className="card-link">Chi tiết công trình</Link>
-								</div>
-								</Popup>
-							</Marker>
-						))
-						: ""
-					}
-				</LayersControl>
-
-				
+				<BasemapLayer name="Imagery" />
+				<BasemapLayer name="ImageryLabels" />
 			</MapContainer>
 		);
     }
